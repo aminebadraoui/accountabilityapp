80c4e67118a5ad770947ab8246ec6aa3cdcd8fa8
Amine Badraoui / Mo Sersouri <a_badraoui@MTL1-W-MB-20399.local>
2020-11-20 10:48:22 -0500

    Project setup

---
+++ AccountabilityApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccountabilityApp
+//
+//  Created by Amine Badraoui on 2020-11-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
