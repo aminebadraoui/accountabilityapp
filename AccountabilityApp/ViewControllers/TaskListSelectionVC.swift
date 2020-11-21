0cb7f1e85b3ee2ee9aa905a5bae0424f3ff02a7a
Amine Badraoui <a_badraoui@MTL1-W-MB-20399.local>
2020-11-21 13:36:21 -0500

    Add tableView to the TaskListSelectionVC

---
+++ AccountabilityApp/ViewControllers/TaskListSelectionVC.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  AccountabilityApp
+//
+//  Created by Amine Badraoui on 2020-11-20.
+//
+
+import UIKit
+
+enum Row {
+    case firstName, lastName, bio
+}
+
+class TaskListSelectionVC: UIViewController {
+    @IBOutlet weak var taskListTableView: UITableView!
+    
+    var tasks = ["Wake up by 8", "Study", "Workout", "Skill Work", "Mobility", "No Fap"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .cyan
+        
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        self.taskListTableView.backgroundColor = .clear
+        self.taskListTableView.delegate = self
+        self.taskListTableView.dataSource = self
+    }
+}
+
+// MARK: TableView Setup
+
+extension TaskListSelectionVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell") as? TaskCell {
+            let taskName = tasks[indexPath.row]
+            cell.configure(with: taskName)
+            
+            
+            
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 88
+    }
+}
+
